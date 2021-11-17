594807cc58e378fe12c25b004e287d8ea262d314
hashir <hashir@node.com>
2021-11-17 15:28:44 +0500

    Project initiated

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../scss/Navbar.scss'
+const Navbar = () => {
+    return (
+        <>
+            <nav className="navbar">
+                hello
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
